97a801c111f68af1423e3f68937a5613696b74a5
mariacuenca <marcuencac@outlook.com>
2023-07-11 19:33:55 -0300

    ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,31 @@
+//1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar.
+
+//let numero;
+//numero = prompt ("Ingrese un numero")
+//if (numero %2==0){
+//    console.log("El numero " + numero + " es par")
+//}else{
+//    console.log("El numero " + numero + " es impar")
+//}
+
+//2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.
+
+//3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
+
+//let numero;
+//numero=prompt ("Ingrese un numero")
+//if (numero > 0 && numero % 5 == 0){
+//    console.log("el numero " + numero + " es multiplo de 5")
+//}else {
+//    console.log("el numero " + numero + " no es multiplo de 5")
+//}
+
+//4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
+
+//5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
+
+//6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
+
+//7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0
+
+//8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
